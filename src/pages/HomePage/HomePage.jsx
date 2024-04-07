e4a3570375ee7817cc2c060229927786e5d31444
Bektur <bekturnurlanov01@gmail.com>
2024-04-07 17:31:25 +0600

    added fonts

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./HomePage.module.css";
+
+export default function HomePage() {
+  return <div>HomePage</div>;
+}
---
+++ src/pages/TourPage/TourPage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./TourPage.module.css";
+
+export default function TourPage() {
+  return <div>TourPage</div>;
+}
